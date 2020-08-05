5e7b19ebd967960a8ec09c695148110a75d9f1b5
Susie Kim <susie.kim@rewardops.com>
2020-08-05 18:03:10 -0400

    Create first mutation and query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // async items(parent, args, ctx, info) {
+  //   console.log("getting items");
+  //   const items = await ctx.db.query.items();
+
+  //   return items;
+  // },
+
+  items: forwardTo("db"),
+};
 
 module.exports = Query;

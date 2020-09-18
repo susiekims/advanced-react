a9b353be68cc11e052f37b3af7d3b5b1114f5755
Susie Kim <susie.kim@rewardops.com>
2020-09-18 09:47:54 -0400

    Cart component vid 39

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,18 +1,19 @@
 import Nav from "./Nav";
 import Link from "next/link";
 import styled from "styled-components";
-import Router from 'next/router';
-import NProgress from 'nprogress';
+import Router from "next/router";
+import NProgress from "nprogress";
+import Cart from "./Cart";
 
 Router.onRouteChangeStart = () => {
-  NProgress.start()
-}
+  NProgress.start();
+};
 Router.onRouteChangeComplete = () => {
   NProgress.done();
-}
+};
 Router.onRouteChangeError = () => {
   NProgress.done();
-}
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -48,7 +49,7 @@ const StyledHeader = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 1px solid ${props => props.theme.lightGrey}
+    border-bottom: 1px solid ${(props) => props.theme.lightGrey};
   }
 `;
 
@@ -65,7 +66,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 

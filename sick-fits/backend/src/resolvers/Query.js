bf664184f83ef0d77d426379d09cefc25c63c2a3
Susie Kim <susie.kim@rewardops.com>
2020-09-30 11:01:10 -0400

    Orders page vid 53

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,5 +1,6 @@
 const { forwardTo } = require("prisma-binding");
 const { hasPermission } = require("../utils");
+const { createOrder } = require("./Mutation");
 
 const Query = {
   items: forwardTo("db"),
@@ -26,7 +27,46 @@ const Query = {
 
     return ctx.db.query.users({}, info);
   },
-  
+
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error("You aren't logged in");
+    }
+
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error("You cannot look at this order");
+    }
+
+    return order;
+  },
+
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+
+    if (!userId) {
+      throw new Error("You must be logged in");
+    }
+
+    return ctx.db.query.orders({
+      where: {
+        user: {
+          id: userId
+        },
+      },
+    }, info);
+  },
 };
 
 module.exports = Query;
--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -10,6 +10,7 @@ import User from "./User";
 import CartItem from "./CartItem";
 import calcTotalPrice from "../lib/calcTotalPrice";
 import formatMoney from "../lib/formatMoney";
+import TakeMyMoney from "./TakeMyMoney";
 
 export const LOCAL_STATE_QUERY = gql`
   query {
@@ -56,7 +57,11 @@ const Cart = () => {
             </ul>
             <footer>
               <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-              <SickButton>Checkout</SickButton>
+              {me.cart.length && (
+                <TakeMyMoney>
+                  <SickButton>Checkout</SickButton>
+                </TakeMyMoney>
+              )}
             </footer>
           </CartStyles>
         );

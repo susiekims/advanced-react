2511d91539a27d73ebec1dbc103744dcdf797eec
Susie Kim <susie.kim@rewardops.com>
2020-10-15 10:08:30 -0400

    Order tests vid 65

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -13,7 +13,7 @@ const totalItems = (cart) => {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 };
 
-const CREATE_ORDER_MUTATION = gql`
+export const CREATE_ORDER_MUTATION = gql`
   mutation createOrderMutation($token: String!) {
     createOrder(token: $token) {
       id
@@ -29,8 +29,7 @@ const CREATE_ORDER_MUTATION = gql`
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
-    console.log(res);
-    console.log(res.id);
+    NProgress.start();
 
     const order = await createOrder({
       variables: {
@@ -47,29 +46,32 @@ class TakeMyMoney extends React.Component {
   render() {
     return (
       <User>
-        {({ data: { me } }) => (
-          <Mutation
-            mutation={CREATE_ORDER_MUTATION}
-            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-          >
-            {(createOrder) => (
-              <StripeCheckout
-                amount={calcTotalPrice(me.cart)}
-                name="Sick Fits"
-                description={`Order of ${totalItems(me.cart)} items`}
-                image={
-                  me.cart.length && me.cart[0].item && me.cart[0].item.image
-                }
-                stripeKey="pk_test_51HVHJjAdCnETMfbK3KeK2QsL6NQlRCJA0ZoUeO36rHqOMMpWLGySX9EHBpyArd9ynIkHJKB362pMw6PXChZi475z005pxsmXIf"
-                currency="USD"
-                email={me.email}
-                token={(res) => this.onToken(res, createOrder)}
-              >
-                {this.props.children}
-              </StripeCheckout>
-            )}
-          </Mutation>
-        )}
+        {({ data: { me }, loading }) => {
+          if (loading) return null;
+          return (
+            <Mutation
+              mutation={CREATE_ORDER_MUTATION}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+              {(createOrder) => (
+                <StripeCheckout
+                  amount={calcTotalPrice(me.cart)}
+                  name="Sick Fits"
+                  description={`Order of ${totalItems(me.cart)} items`}
+                  image={
+                    me.cart.length && me.cart[0].item && me.cart[0].item.image
+                  }
+                  stripeKey="pk_test_51HVHJjAdCnETMfbK3KeK2QsL6NQlRCJA0ZoUeO36rHqOMMpWLGySX9EHBpyArd9ynIkHJKB362pMw6PXChZi475z005pxsmXIf"
+                  currency="USD"
+                  email={me.email}
+                  token={(res) => this.onToken(res, createOrder)}
+                >
+                  {this.props.children}
+                </StripeCheckout>
+              )}
+            </Mutation>
+          );
+        }}
       </User>
     );
   }
